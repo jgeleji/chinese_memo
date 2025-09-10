21e5367aeeb17158e47af819c6d4b2dec0517835
Geleji Janos <janos@geleji.hu>
2025-09-10 21:08:04 +0200

    rename to ncurses

--- comp.sh
+++ comp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #COMPILER=g++
 COMPILER=clang++
-${COMPILER} input.cpp main_input.cpp tokenize.cpp -lncurses -o input
-${COMPILER} input.cpp main_questions.cpp tokenize.cpp questions.cpp -lncurses -o questions
-${COMPILER} autoex.cpp tokenize.cpp input.cpp -lncurses -o autoex
-${COMPILER} input.cpp main_characters.cpp tokenize.cpp questions.cpp -lncurses -o characters
-${COMPILER} input.cpp main_characters_easy.cpp tokenize.cpp questions.cpp -lncurses -o characters_easy
+${COMPILER} input_ncurses.cpp main_input.cpp tokenize.cpp -lncurses -o input
+${COMPILER} input_ncurses.cpp main_questions.cpp tokenize.cpp questions.cpp -lncurses -o questions
+${COMPILER} autoex.cpp tokenize.cpp input_ncurses.cpp -lncurses -o autoex
+${COMPILER} input_ncurses.cpp main_characters.cpp tokenize.cpp questions.cpp -lncurses -o characters
+${COMPILER} input_ncurses.cpp main_characters_easy.cpp tokenize.cpp questions.cpp -lncurses -o characters_easy
 cat words_* | sort | uniq > union_q.txt
 ./autoex words_*
 cat autoex_words_* | sort | uniq > union_a.txt
